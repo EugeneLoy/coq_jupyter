ace8a6674b2b4e3fa67c19111f943129cd539bfb
Eugene Loy <eugeny.loy@gmail.com>
2019-01-02 15:28:12 +0200

    implemented using coqtop in -emacs mode

--- coq_jupyter/kernel.py
+++ coq_jupyter/kernel.py
@@ -1,16 +1,17 @@
 import pexpect
 import re
+import random
 
 from ipykernel.kernelbase import Kernel
 from subprocess import check_output
-
+from operator import itemgetter
 
 __version__ = '1.0.0'
 
-INITIAL_PROMPT = u".*Coq\s\<\s"
-PROMPT = u"([^\s]+)\s\<\s" # TODO add match for newline here
+PROMPT = u"\<prompt\>.+?\s\<\s(?P<state_label>\d+)\s\|(?P<proving>.*?)\|\s\d+\s\<\s\<\/prompt\>"
 
 LANGUAGE_VERSION_PATTERN = re.compile(r'version (\d+(\.\d+)+)')
+ANSI_ESCAPE_PATTERN = re.compile(r'(\x9B|\x1B\[)[0-?]*[ -/]*[@-~]') # see: https://stackoverflow.com/a/38662876/576549
 
 
 class CoqKernel(Kernel):
@@ -39,34 +40,133 @@ class CoqKernel(Kernel):
     def language_version(self):
         return LANGUAGE_VERSION_PATTERN.search(self.banner).group(1)
 
+    _state_labbel = None
 
     def __init__(self, **kwargs):
         Kernel.__init__(self, **kwargs)
         self._start_coqtop()
 
     def _start_coqtop(self):
-        self._coqtop = pexpect.spawn('coqtop', echo=False, encoding='utf-8')
-        self._coqtop.expect(INITIAL_PROMPT, None)
+        self._coqtop = pexpect.spawn(u"coqtop -emacs -quiet", echo=False, encoding=u"utf-8")
+        self._state_labbel = "1"
+        self._eval(u"(* dummy init command *)")
 
     def _eval(self, code):
-        lines = [line for line in code.splitlines() if line.strip("\n\r\t ") != u""]
+        rollback_state_label = self._state_labbel
+        checkpoint_marker_lhs = random.randint(0, 499)
+        checkpoint_marker_rhs = random.randint(0, 499)
+        checkpoint_marker = str(checkpoint_marker_lhs + checkpoint_marker_rhs)
+        eval_checkpoint_command = u"Compute {} + {}.".format(checkpoint_marker_lhs, checkpoint_marker_rhs)
+        hidden_commands_issued = 1
+
+        # attempt evaluation
+        self._coqtop.send(u"{}\n{}\n".format(code, eval_checkpoint_command))
+
+        # collect evaluation output
         outputs = []
+        while True:
+            (prompt_match, raw_output, simplified_output, error_message) = self._expect_coqtop_prompt()
+            outputs.append((prompt_match, raw_output, simplified_output, error_message))
+
+            if checkpoint_marker in simplified_output:
+                self.log.debug(u"checkpoint reached")
+                break
+
+            if error_message and eval_checkpoint_command in simplified_output:
+                self.log.debug(u"checkpoint eval failed")
+                break
+
+        # query proof state if proving something now
+        if outputs[-1][0].group(u"proving") != u"":
+            self._coqtop.sendline(u"Show.")
+            outputs.append(self._expect_coqtop_prompt())
+            hidden_commands_issued += 1
+
+        # do full cell rollback if error were detected
+        if any(map(itemgetter(3), outputs)):
+            self._coqtop.sendline(u"BackTo {}.".format(rollback_state_label))
+            self._expect_coqtop_prompt()
+
+            raw_outputs = []
+            footer_message = "Cell evaluation error: some of the commands resulted in error. Cell rolled back."
+            for (_, raw_output, simplified_output, _) in outputs:
+                if eval_checkpoint_command in simplified_output:
+                    footer_message = "Cell evaluation error: last cell command is incomplete or malformed. Cell rolled back."
+                    break
+
+                if checkpoint_marker in simplified_output:
+                    break
+
+                raw_outputs.append(simplified_output)
+
+            return self._render_cell_output(raw_outputs, footer_message)
+
+        # rollback issued hidden commands
+        # TODO not 100% sure if this rollback is really needed. Leave it for now.
+        self._coqtop.sendline(u"BackTo {}.".format(outputs[-hidden_commands_issued][0].group(u"state_label")))
+        self._expect_coqtop_prompt()
+
+        # treat state after hidden commands rollback as 'commited'
+        self._state_labbel = self._coqtop.match.group(u"state_label")
+
+        # build cell output message
+        raw_outputs = list(map(itemgetter(1), outputs))
+        del raw_outputs[-hidden_commands_issued] # omit checkpoint marker output
+
+        if self._coqtop.match.group(u"proving") != u"":
+            footer_message = u"Proving: {}".format(self._coqtop.match.group(u"proving"))
+        else:
+            footer_message = None
+
+        return self._render_cell_output(raw_outputs, footer_message)
+
+    def _render_cell_output(self, raw_outputs, footer_message):
+        cell_output = u"\n".join(raw_outputs)
+        cell_output = cell_output.rstrip(u"\n\r\t ")
+
+        if footer_message is not None:
+            cell_output += "\n" + footer_message
+
+        return cell_output
+
+    def _expect_coqtop_prompt(self):
+        self.log.debug(u"expecting coqtop prompt")
+        self._coqtop.expect(PROMPT, None)
+
+        prompt_match = self._coqtop.match
+        self.log.debug(u"coqtop prompt: {}".format(repr(prompt_match)))
+
+        raw_output = self._coqtop.before
+        self.log.debug(u"coqtop output (raw): {}".format(repr(raw_output)))
+
+        simplified_output = self._simplify_output(raw_output)
+        self.log.debug(u"coqtop output (simplified): {}".format(repr(simplified_output)))
+
+        error_message = self._is_error_output(simplified_output)
+        self.log.debug(u"coqtop output contains error message: {}".format(error_message))
+
+        return (prompt_match, raw_output, simplified_output, error_message)
+
+    def _simplify_output(self, output):
+        # clean colors
+        output = ANSI_ESCAPE_PATTERN.sub(u"", output)
+        # replace \n\r with \n
+        output = u"\n".join(output.splitlines())
+        return output.strip(u"\n\t ")
+
+    def _is_error_output(self, output):
+        lines = output.splitlines()
+
+        if len(lines) == 0:
+            return False
+
+        error_location_found = False
         for line in lines:
-            self._coqtop.sendline(line)
-            self._coqtop.expect(PROMPT, None)
-            outputs.append(self._coqtop.before);
+            error_location_found = error_location_found or line.startswith("Toplevel input, characters")
+            if error_location_found and (line.startswith("Error:") or line.startswith("Syntax error:")):
+                return True
 
-        # Parse "proving" context from prompt
-        prompt_context = self._coqtop.match.group(1)
-        if prompt_context != u"Coq":
-            outputs.append(u"Proving: {}".format(prompt_context))
-
-        return u"\n".join(outputs);
-
-        # TODO cleanup:
-        # self._coqtop.sendline(u" ".join(code.splitlines()))
-        # self._coqtop.expect("[^\s]+\s\<\s", None)
-        # return self._coqtop.before
+        return False
 
     def _build_ok_content(self):
         return {
@@ -93,13 +193,17 @@ class CoqKernel(Kernel):
 
     def do_execute(self, code, silent, store_history=True, user_expressions=None, allow_stdin=False):
         try:
-            self.log.info("Processing 'execute_request', code: \n{}\n".format(repr(code)))
-            result = self._eval(code)
-            self.log.info("Sending 'execute_result', evaluation result: \n{}\n".format(repr(result)))
-            self._send_execute_result(result)
+            self.log.info(u"Processing 'execute_request', code: \n{}\n".format(repr(code)))
+            if code.strip(u"\n\r\t ") != u"":
+                result = self._eval(code)
+                self.log.info(u"Sending 'execute_result', evaluation result: \n{}\n".format(repr(result)))
+                self._send_execute_result(result)
+            else:
+                self.log.info(u"code is empty - skipping evaluation and sending results.")
+
             return self._build_ok_content()
         except Exception as e:
-            self.log.exception("Error during evaluating code: \n'{}'\n".format(repr(code)))
+            self.log.exception(u"Error during evaluating code: \n'{}'\n".format(repr(code)))
             return self._build_error_content(e)
 
 # This entry point is used for debug only:
--- test/kernel_test.py
+++ test/kernel_test.py
@@ -7,15 +7,191 @@ class KernelTests(jupyter_kernel_test.KernelTests):
     kernel_name = "coq"
     language_name = "coq"
 
-    def test_coq_jupyter_execute_one_line(self):
+    def _build_sum_command(self, lhs, rhs):
+        result = str(lhs + rhs)
+        command = "Compute {} + {}.".format(lhs, rhs)
+        return (result, command)
+
+    def _execute_cell(self, code):
         self.flush_channels()
-        reply, output_msgs = self.execute_helper(code='Check True.')
+        reply, output_msgs = self.execute_helper(code=code)
 
         self.assertEqual(reply['content']['status'], 'ok')
         self.assertEqual(len(output_msgs), 1)
         self.assertEqual(output_msgs[0]['msg_type'], 'execute_result')
         self.assertIn('text/plain', output_msgs[0]['content']['data'])
-        self.assertIn("Prop", output_msgs[0]['content']['data']['text/plain'])
+
+        return output_msgs[0]['content']['data']['text/plain']
+
+
+    def test_coq_jupyter____executing_empty_code____should_not_print_anything(self):
+        self.flush_channels()
+        reply, output_msgs = self.execute_helper(code="")
+
+        self.assertEqual(reply['content']['status'], 'ok')
+        self.assertEqual(len(output_msgs), 0)
+
+    def test_coq_jupyter____executing_code_without_coq_content____should_not_print_anything(self):
+        self.flush_channels()
+        reply, output_msgs = self.execute_helper(code="\n\r\t \n\r\t ")
+
+        self.assertEqual(reply['content']['status'], 'ok')
+        self.assertEqual(len(output_msgs), 0)
+
+    def test_coq_jupyter____executing_one_command____prints_computed_command_result(self):
+        (expected_result, command) = self._build_sum_command(100, 1)
+        result = self._execute_cell(command)
+        self.assertIn(expected_result, result)
+
+    def test_coq_jupyter____executing_one_command____does_not_print_command(self):
+        (expected_result, command) = self._build_sum_command(100, 2)
+        result = self._execute_cell(command)
+        self.assertNotIn(command, result)
+
+    def test_coq_jupyter____executing_one_command____does_not_print_hidden_checkpoint_command_and_its_result(self):
+        result = self._execute_cell("Check True.")
+        hidden_checkpoint_command_part = "Compute"
+        hidden_checkpoint_command_result_part = "nat"
+        self.assertNotIn(hidden_checkpoint_command_part, result)
+        self.assertNotIn(hidden_checkpoint_command_result_part, result)
+
+    def test_coq_jupyter____executing_compute_command_when_not_proving____does_not_print_proving_context(self):
+        (expected_result, command) = self._build_sum_command(100, 3)
+        result = self._execute_cell(command)
+        self.assertNotIn("Proving:", result)
+        self.assertNotIn("subgoal", result)
+
+    def test_coq_jupyter____executing_multiple_commands____prints_computed_command_results(self):
+        (expected_result1, command1) = self._build_sum_command(100, 4)
+        (expected_result2, command2) = self._build_sum_command(100, 5)
+        result = self._execute_cell(command1 + " " + command2)
+        self.assertIn(expected_result1, result)
+        self.assertIn(expected_result2, result)
+
+    def test_coq_jupyter____executing_commands_when_proving____prints_proving_context(self):
+        result = self._execute_cell("Theorem t1 : True.")
+        self.assertIn("1 subgoal", result)
+        self.assertIn("Proving: t1", result)
+
+        result = self._execute_cell(self._build_sum_command(100, 6)[1])
+        self.assertIn("1 subgoal", result)
+        self.assertIn("Proving: t1", result)
+
+        result = self._execute_cell("Proof. pose (i1 := I).")
+        self.assertIn("1 subgoal", result)
+        self.assertIn("i1 := I : True", result)
+        self.assertIn("Proving: t1", result)
+
+        result = self._execute_cell(self._build_sum_command(100, 7)[1])
+        self.assertIn("1 subgoal", result)
+        self.assertIn("i1 := I : True", result)
+        self.assertIn("Proving: t1", result)
+
+        result = self._execute_cell("exact i1. Qed.")
+        self.assertNotIn("1 subgoal", result)
+        self.assertNotIn("No more subgoals", result)
+        self.assertNotIn("i1 := I : True", result)
+        self.assertNotIn("Proving:", result)
+
+        result = self._execute_cell(self._build_sum_command(100, 8)[1])
+        self.assertNotIn("1 subgoal", result)
+        self.assertNotIn("No more subgoals", result)
+        self.assertNotIn("i1 := I : True", result)
+        self.assertNotIn("Proving:", result)
+
+    def test_coq_jupyter____when_proving____does_not_print_hidden_commands_and_their_results(self):
+        result = self._execute_cell("Theorem t2 : True.")
+
+        hidden_checkpoint_command_part = "Compute"
+        hidden_checkpoint_command_result_part = "nat"
+        hidden_show_command = "Show."
+
+        self.assertNotIn(hidden_checkpoint_command_part, result)
+        self.assertNotIn(hidden_checkpoint_command_result_part, result)
+        self.assertNotIn(hidden_show_command, result)
+        # Note: the result of Show command actually should be shown, so there is no need to verify its absense
+
+        self._execute_cell("Proof. exact I. Qed.") # TODO move cleanup to teardown
+
+    def test_coq_jupyter____when_proving____prints_most_recent_proving_context_once(self):
+        result = self._execute_cell("Theorem t3 : bool. Proof. pose (b1 := true). pose (b2 := false).")
+        self.assertEqual(result.count("Proving: t3"), 1, "result: " + repr(result))
+        self.assertEqual(result.count("1 subgoal"), 1, "result: " + repr(result))
+        self.assertEqual(result.count("No more subgoals"), 0, "result: " + repr(result))
+        self.assertEqual(result.count("b1 := true : bool"), 1, "result: " + repr(result))
+        self.assertEqual(result.count("b2 := false : bool"), 1, "result: " + repr(result))
+
+        result = self._execute_cell("exact b2.")
+        self.assertEqual(result.count("Proving: t3"), 1, "result: " + repr(result))
+        self.assertEqual(result.count("1 subgoal"), 0, "result: " + repr(result))
+        self.assertEqual(result.count("No more subgoals"), 1, "result: " + repr(result))
+        self.assertEqual(result.count("b1 := true : bool"), 0, "result: " + repr(result))
+        self.assertEqual(result.count("b2 := false : bool"), 0, "result: " + repr(result))
+
+        self._execute_cell("Qed.") # TODO move cleanup to teardown
+
+    def _build_commands_with_error_fixture(self, t_base, t, valid_command_template, invalid_command_template, expected_error_message):
+        return (
+            "t{}_{}".format(t_base + t, t),
+            ["t{}_{}".format(t_base + t, i) for i in (1, 2, 3) if i != t],
+            ("t{}_0".format(t_base + t), valid_command_template.format(t_base + t, 0)),
+            "\n".join([
+                valid_command_template.format(t_base + t, i) if i != t else invalid_command_template.format(t_base + t, i)
+                for i in (1, 2, 3)
+            ]),
+            expected_error_message
+        )
+
+    def test_coq_jupyter____executing_commands_with_error____prints_error_and_rolls_back(self):
+        cases_with_reference_error = [
+            self._build_commands_with_error_fixture(
+                3,
+                t,
+                "Definition t{}_{} := I.",
+                "Definition t{}_{} := INVALID_REFERENCE.",
+                "Error: The reference INVALID_REFERENCE was not found"
+            )
+            for t in (1,2,3)
+        ]
+        cases_with_syntax_error = [
+            self._build_commands_with_error_fixture(
+                6,
+                t,
+                "Definition t{}_{} := I.",
+                "Definition t{}_{} := (I.",
+                "Syntax error: ',' or ')' expected after [constr:operconstr"
+            )
+            for t in (1,2,3)
+        ]
+        cases_with_incomplete_command_error = [
+            self._build_commands_with_error_fixture(
+                9,
+                t,
+                "Definition t{}_{} := I.",
+                "Definition t{}_{} := I",
+                # missing "." in last command may cause hidden command to appear in error message.
+                # In this case alternative footer explaining error should be shown instead of coqtop error output
+                "Syntax error: '.' expected after" if t != 3 else "Cell evaluation error: last cell command is incomplete or malformed."
+            )
+            for t in (1,2,3)
+        ]
+        fixture = cases_with_reference_error + cases_with_syntax_error + cases_with_incomplete_command_error
+
+        for f in range(len(fixture)):
+            (invalid_definition, valid_definitions, commited_definition, code, expected_error_message) = fixture[f]
+
+            self._execute_cell(commited_definition[1])
+
+            result = self._execute_cell(code)
+            self.assertIn("cell rolled back", result.lower(), "fixture: {}".format(repr(fixture[f])))
+            self.assertIn(expected_error_message, result, "fixture: {}".format(repr(fixture[f])))
+
+            # verify rollback
+            result = self._execute_cell("Print All.")
+            self.assertIn(commited_definition[0], result, "fixture: {}".format(repr(fixture[f])))
+            self.assertNotIn(invalid_definition, result, "fixture: {}".format(repr(fixture[f])))
+            self.assertNotIn(valid_definitions[0], result, "fixture: {}".format(repr(fixture[f])))
+            self.assertNotIn(valid_definitions[1], result, "fixture: {}".format(repr(fixture[f])))
 
 
 if __name__ == '__main__':
