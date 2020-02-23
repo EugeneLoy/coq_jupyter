a20c279f7364ac6f100f9a46929a995a0f49c83e
Eugene Loy <Eugeny.Loy@gmail.com>
2020-02-23 12:48:29 +0200

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,8 +1,30 @@
 from wexpect import spawn
 
-child = spawn('cmd.exe')
-child.expect('>')
-child.sendline('ls')
-child.expect('>')
-print(child.before)
-child.sendline('exit')
+spawn_args = {
+    "echo": False
+    #"encoding": "utf-8",
+    #"codec_errors": "replace"
+}
+
+INIT_COMMAND = '<call val="Init"> <option val="none"/> </call>'
+
+REPLY_PATTERNS = [
+    re.compile(r'\<{0}.*?\>.+?\<\/{0}\>'.format(t), re.DOTALL)
+    for t in [
+        "feedback",
+        "value",
+        "message" # older versions of coqtop wont wrap 'message' inside 'feedback'
+    ]
+]
+
+c = spawn("{} -main-channel stdfds {}".format("coqidetop", ""), **spawn_args)
+c.send(command + "\n")
+c.expect(REPLY_PATTERNS)
+print(c.before)
+
+#child = spawn('cmd.exe')
+#child.expect('>')
+#child.sendline('ls')
+#child.expect('>')
+#print(child.before)
+#child.sendline('exit')
