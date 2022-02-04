f9953d4d1e2083b025bfd25c487fbaddcc133654
Eugene Loy <eugeny.loy@gmail.com>
2022-02-04 22:38:18 +0200

    CI configuration overhaul

---
+++ .github/actions/coq-docker-container/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export OPAMROOT=/home/coq/.opam
+eval $(opam env)
+
+python3 --version
+coqtop --version
+
+sudo pip3 install --upgrade --force-reinstall /github/workspace/dist/coq_jupyter-*.tar.gz 'jupyter_client<=6.1.12' 'jupyter_kernel_test<=0.3'
+sudo python3 -m coq_jupyter.install
+
+python3 /github/workspace/test/kernel_test.py -v
